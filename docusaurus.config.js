1346f727ceda6d0082ff5299ed77b04e1bed0c41
Tobias Borowski <tobias.borowski@googlemail.com>
2025-09-01 21:26:41 +0200

    add config for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,6 +29,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'beanssmart', // Usually your GitHub org/user name.
   projectName: 'smartbeans-doc', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
