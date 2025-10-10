5aaee554170de81fb35fb6b871855ccde03ea930
Tobias Borowski <tobias.borowski@googlemail.com>
2025-10-10 19:37:44 +0200

    remove deprecated entry

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,6 @@ const config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
